26a0387365260338a0ffcb3a142e79f8867cd796
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-03-03 10:52:17 +0000

    Add class to represent Transactions Explorer data

---
+++ collector/classes/transactions_explorer_data_type.py
@@ -0,0 +1,20 @@
+class TransactionsExplorerDataType(object):
+    """Represents a type of data that is stored in Transactions Explorer"""
+    def __init__(self, title, key_format, metrics, periods):
+        self.title = title
+        self.key_format = key_format
+        self.metrics = metrics
+        self.periods = periods
+
+    def get_key(self, metric, period):
+        """Returns the row title (key) based on the format used"""
+        key = self.key_format
+        key = key.replace('{metric}', metric)
+        key = key.replace('{period}', period)
+        return key
+
+    def get_period_start_date(self, period_key):
+        return self.periods[period_key]['_start_at']
+
+    def get_period_end_date(self, period_key):
+        return self.periods[period_key]['_end_at']

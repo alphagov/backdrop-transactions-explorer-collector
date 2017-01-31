b3bd500cd7e8127e83f6521251860ebab0013b1d
Lee Longmore <lee.longmore@gmail.com>
2017-01-31 12:01:45 +0000

    Prepare for Q2 2016 Tx Explorer data import

--- collector/process.py
+++ collector/process.py
@@ -96,6 +96,10 @@ def setup_data_types():
                 '_start_at': datetime.datetime(2015, 01, 01, 0, 0),
                 '_end_at': datetime.datetime(2016, 01, 01, 0, 0),
             },
+            '2016-Q2': {
+                '_start_at': datetime.datetime(2015, 04, 01, 0, 0),
+                '_end_at': datetime.datetime(2016, 04, 01, 0, 0),
+            },
         },
         'year'
     )
@@ -169,6 +173,10 @@ def setup_data_types():
             'Oct - Dec 2015': {
                 '_start_at': datetime.datetime(2015, 10, 01, 0, 0),
                 '_end_at': datetime.datetime(2016, 01, 01, 0, 0),
+            },
+            'Jan - Mar 2016': {
+                '_start_at': datetime.datetime(2016, 01, 01, 0, 0),
+                '_end_at': datetime.datetime(2016, 04, 01, 0, 0),
             },
         },
         'quarter'

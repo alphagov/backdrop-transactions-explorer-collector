6f1b5b1e1047b0199bb9b08fb6cccc30930f39e2
ambrozic <ambrozic@gmail.com>
2017-05-23 10:35:38 +0100

    Moved sections appart

--- jenkins-data.sh
+++ jenkins-data.sh
@@ -9,17 +9,13 @@ virtualenv ./virtualenv/
 ./virtualenv/bin/pip install --upgrade pip wheel
 ./virtualenv/bin/pip install -r requirements.txt
 
-echo "Collecting data from the spreadsheet"
-
+echo "Setting up configuration"
 echo "{\"transactions_explorer\": {\"credentials\": $GOOGLE_CREDENTIALS,\"key\": \"$GOOGLE_SPREADSHEET_KEY\",\"worksheet\": \"$GOOGLE_SPREADSHEET_WORKSHEET\" } }" > config.json
 
-# disabling warnings because an old version of python and urllib3. script otherwise exits.
-# https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
-export PYTHONWARNINGS="ignore"
+echo "Collecting data from the spreadsheet"
 ./backdrop-transactions-explorer-collector transactions_explorer config.json > transactions-explorer-data.json
 
 echo "Posting data to Backdrop"
-
 curl --fail $BACKDROP_ENDPOINT --request POST --header "Authorization: Bearer $BACKDROP_TOKEN" --header 'Content-type: application/json' --data @transactions-explorer-data.json
 
 echo "Done."
